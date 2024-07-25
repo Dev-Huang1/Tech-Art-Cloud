fe75c7465e5367a81a8b5d08a2459079f235ed91
Dev.Huang <devhuang000@outlook.com>
2024-07-25 16:39:36 +0800

    Update base.js

--- assets/js/base.js
+++ assets/js/base.js
@@ -34,24 +34,3 @@ document.addEventListener('DOMContentLoaded', function() {
         `;
         return card;
     }
-
-    function generateRandomString(length) {
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789#%@*&';
-        let result = '';
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return result;
-    }
-
-    function onTurnstileSuccess(token) {
-        const randomString = generateRandomString(300);
-        const newUrl = window.location.href.split('?')[0] + '?cf-turnstile-code=' + randomString;
-        window.history.replaceState(null, null, newUrl);
-        
-        setTimeout(function() {
-            window.history.replaceState(null, null, window.location.pathname);
-            document.getElementById('captcha-container').classList.add('hidden');
-            document.getElementById('original-content').classList.remove('hidden');
-        }, 1869);
-    }
